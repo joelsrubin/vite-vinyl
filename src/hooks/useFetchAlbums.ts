53c6279e651a5d345db3d0574b50dcc049ecf946
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-07-17 15:58:52 -0500

    refactor recursive to Promise.all

--- src/hooks/useFetchAlbums.ts
+++ src/hooks/useFetchAlbums.ts
@@ -3,7 +3,7 @@ import { useMutation } from "react-query";
 const KEY = "QjMwskBJVDuuVwHxmJuI";
 const SECRET = "oJQZXITXAiAYDBdzvOUgdKIoWnTLvlQn";
 
-async function recursiveFetch(url: string): Promise<Release[]> {
+async function paginatedFetch(url: string): Promise<Release[]> {
   const response = await fetch(url);
   const data = await response.json();
 
@@ -11,20 +11,32 @@ async function recursiveFetch(url: string): Promise<Release[]> {
     throw new Error("User does not exist or may have been deleted.");
   }
 
-  if (data.pagination.urls.next) {
-    const nextData = await recursiveFetch(data.pagination.urls.next);
-    return [...data.releases, ...nextData];
+  let albums: Release[] = [...data.releases];
+  if (data.pagination.pages > 1) {
+    let urls = [];
+
+    for (let i = 1; i < data.pagination.pages; i++) {
+      urls.push(`${url}&page=${i + 1}`);
+    }
+
+    await Promise.all(
+      urls.map(async (u) => {
+        const response = await fetch(u);
+        const data = await response.json();
+        albums.push(...data.releases);
+      })
+    );
   }
-  return data.releases;
+  return albums;
 }
 
 const fetchAlbums = async (userName: string): Promise<Release[]> => {
   const URL = `https://api.discogs.com/users/${userName}/collection/folders/0/releases?&per_page=250&key=${KEY}&secret=${SECRET}`;
   try {
-    const albums = await recursiveFetch(URL);
+    const albums = await paginatedFetch(URL);
     return albums;
   } catch (e) {
-    throw new Error("User does not exist or may have been deleted.");
+    throw e;
   }
 };
 
