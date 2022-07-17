4359fb2c9e92015397b0caf85b26e2c2fc3ab8ab
Joel Rubin <joelsrubin@joelrubxqw3vh0k.lan>
2022-07-17 14:05:32 -0500

    add recursive search

--- src/hooks/useFetchAlbums.ts
+++ src/hooks/useFetchAlbums.ts
@@ -3,22 +3,28 @@ import { useMutation } from "react-query";
 const KEY = "QjMwskBJVDuuVwHxmJuI";
 const SECRET = "oJQZXITXAiAYDBdzvOUgdKIoWnTLvlQn";
 
-const fetchAlbums = async (userName: string): Promise<Release[]> => {
-  const data = await fetch(
-    `https://api.discogs.com/users/${userName}/collection/folders/0/releases?&per_page=500&key=${KEY}&secret=${SECRET}`
-  );
+async function recursiveFetch(url: string): Promise<Release[]> {
+  const response = await fetch(url);
+  const data = await response.json();
 
-  const json = await data.json();
-  let albums = [...json.releases];
-  if (json.pagination.urls.next) {
-    const nextData = await fetch(json.pagination.urls.next);
-    albums = [...albums, ...(await nextData.json()).releases];
+  if (data.message) {
+    throw new Error("User does not exist or may have been deleted.");
   }
 
-  if (json.message) {
-    throw new Error("User does not exist or may have been deleted.");
-  } else {
+  if (data.pagination.urls.next) {
+    const nextData = await recursiveFetch(data.pagination.urls.next);
+    return [...data.releases, ...nextData];
+  }
+  return data.releases;
+}
+
+const fetchAlbums = async (userName: string): Promise<Release[]> => {
+  const URL = `https://api.discogs.com/users/${userName}/collection/folders/0/releases?&per_page=250&key=${KEY}&secret=${SECRET}`;
+  try {
+    const albums = await recursiveFetch(URL);
     return albums;
+  } catch (e) {
+    throw new Error("User does not exist or may have been deleted.");
   }
 };
 
